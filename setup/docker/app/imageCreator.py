c8f67a661fc12e9a70531503b345b20b447fe601
sdanthinne <sdanthinne@gmail.com>
2021-02-10 16:09:34 -0800

    added image creator dispaly

--- setup/docker/app/imageCreator.py
+++ setup/docker/app/imageCreator.py
@@ -1,4 +1,17 @@
 #this file contains methods to create histogram images from an array of tuples with (date, temperature)
+from matplotlib import pyplot as plt
+from multiprocessing import Process
+import os
 
-def createHistogram(data, endLocation):
 
+def createHistogram(data, endLocation,identifier):
+    #data 
+    plt.figure(figsize=[10,8])
+    plt.title(identifier.toString())
+    plt.xlabel('Date',fontsize=15)
+    ply.ylabel('Temperature',fontsize=15)
+    plt.plot(data[0],data[1])
+    #save the created plot
+    plt.savefig(endLocation,bbox_inches='tight')
+
+def 
