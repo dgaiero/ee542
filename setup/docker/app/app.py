16b79147a3cf7504d7c9a58ebda2ec01fdbff809
Sebastien Danthinne <sdanthinne@gmail.com>
2021-03-03 17:00:00 -0800

    removed socket garbage

--- setup/docker/app/app.py
+++ setup/docker/app/app.py
@@ -15,7 +15,6 @@ ALLOWED_EXTENSIONS = {'png','jpg','jpeg','gif'}
 app = Flask(__name__)
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
 
-test = True
 
 #tests to see if the filename is valid or not
 def allowed_file(filename):
@@ -93,10 +92,6 @@ def login_postimage(filename):
 if __name__ == "__main__":
     listen_thread = threading.Thread(target = listener)
     listen_thread.start() # start the listening backend. Could do a fork, but either one seems to work.
-    if(test):
-        #create a dummy process to test socket communication
-        process = subprocess.Popen(['python3','testSockets.py'])
-
 
     
     app.run(debug=False, host='0.0.0.0') # start the flask frontend
--- setup/docker/app/imageCreator.py
+++ setup/docker/app/imageCreator.py
@@ -1,4 +1,4 @@
-#this file contains methods to create histogram images from an array of tuples with (date, temperature)
+#this file contains methods to create timeline images from an array of tuples with (date, temperature)
 from matplotlib import pyplot as plt
 from multiprocessing import Process
 import os
@@ -13,6 +13,6 @@ def createHistogram(data, endLocation,identifier):
     ply.ylabel('Temperature',fontsize=15)
     plt.plot(data[0],data[1])
     #save the created plot
-    plt.savefig(outbox,bbox_inches='tight')
+    plt.savefig(outbox+"/"+identifier.toString(),bbox_inches='tight')
 
 
